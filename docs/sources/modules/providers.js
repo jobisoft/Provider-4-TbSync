c9f661cbd85a8d7cd5bc4646800b16122c892293
John Bieling <john.bieling@gmx.de>
2019-10-03 22:22:18 +0200

    upd

--- docs/sources/modules/providers.js
+++ docs/sources/modules/providers.js
@@ -56,6 +56,7 @@ var providers = {
         this.loadedProviders[provider].createAccountWindow = null;
 
         this.loadedProviders[provider].bundle = Services.strings.createBundle(this[provider].Base.getStringBundleUrl());
+        addon.contributorsURL = this[provider].Base.getContributorsUrl();
 
         // check if provider has its own implementation of folderList
         if (!this[provider].hasOwnProperty("folderList")) this[provider].folderList = new TbSync.manager.FolderList(provider);
@@ -103,7 +104,7 @@ var providers = {
           }
         }
         
-		if (TbSync.lightning.isAvailable()) {
+        if (TbSync.lightning.isAvailable()) {
           for (let calendar of TbSync.lightning.cal.getCalendarManager().getCalendars({})) {
             let storedProvider = calendar.getProperty("tbSyncProvider");
             if (provider == storedProvider && calendar.type == "storage" && providerData.getFolders({"target": calendar.id}).length == 0) {
